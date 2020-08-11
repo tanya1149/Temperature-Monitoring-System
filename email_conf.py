427e28532663be7f4987bab341ef1e96ad01dc4d
tanya1149 <57029055+tanya1149@users.noreply.github.com>
2020-08-11 12:38:17 +0530

    Update email_conf.py

--- email_conf.py
+++ email_conf.py
@@ -1,6 +1,8 @@
-MAILGUN_API_KEY = 'e71b9a9db8ab86076a973aec63909b8c-afab6073-de44e91e'
-SANDBOX_URL= 'sandbox1eadeeae3708411980d0ca3354a477fd.mailgun.org'
-SENDER_EMAIL = 'postmaster@sandbox1eadeeae3708411980d0ca3354a477fd.mailgun.org'
-RECIPIENT_EMAIL = 'tanya.gupta.vibgyor@gmail.com'
-API_KEY = 'bb9cbb43-b90a-432a-8bdd-f3982b59a746'
-DEVICE_ID = 'BOLT3851337'
+#Enter your personal details here: first four can be taken from your mailgun account and last two from Bolt cloud
+
+MAILGUN_API_KEY = 'e71bXXXdb8ab86076a973aec63909b8c-afab6073-de44e91e'
+SANDBOX_URL= 'sandbox1eadeeae37084119XXXXca3354a477fd.mailgun.org'
+SENDER_EMAIL = 'postmaster@sandbox1eadeeae37084119XXXXca3354a477fd.mailgun.org'
+RECIPIENT_EMAIL = 'xxxxxxxxxxxxxxxx@gmail.com'
+API_KEY = 'bb9cbb43-b90a-432a-8bdd-XXX82b59a746'
+DEVICE_ID = 'BOLT385XX37'
