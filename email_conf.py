d79da13d13c414b0f8a1034299569db7ea733a18
tanya1149 <57029055+tanya1149@users.noreply.github.com>
2020-08-11 12:30:18 +0530

    Add files via upload

---
+++ email_conf.py
@@ -0,0 +1,6 @@
+MAILGUN_API_KEY = 'e71b9a9db8ab86076a973aec63909b8c-afab6073-de44e91e'
+SANDBOX_URL= 'sandbox1eadeeae3708411980d0ca3354a477fd.mailgun.org'
+SENDER_EMAIL = 'postmaster@sandbox1eadeeae3708411980d0ca3354a477fd.mailgun.org'
+RECIPIENT_EMAIL = 'tanya.gupta.vibgyor@gmail.com'
+API_KEY = 'bb9cbb43-b90a-432a-8bdd-f3982b59a746'
+DEVICE_ID = 'BOLT3851337'
---
+++ temp_conf.py
@@ -0,0 +1,22 @@
+import email_conf
+import json
+import time
+from boltiot import Email, Bolt
+
+minimum_limit = 300
+maximum_limit = 600
+
+mybolt = Bolt(email_conf.API_KEY, email_conf.DEVICE_ID)
+mailer = Email(email_conf.MAILGUN_API_KEY, email_conf.SANDBOX_URL, email_conf.SENDER_EMAIL, email_conf.RECIPIENT_EMAIL)
+while True:
+    response = mybolt.analogRead('A0')
+    data = json.loads(response)
+    print (data['value'])
+    try:
+        sensor_value = int(data['value'])
+        print (sensor_value)
+        if sensor_value > maximum_limit or sensor_value < minimum_limit:
+            response = mailer.send_email("Alert", "The Current temperature sensor value is " +str(sensor_value))
+    except Exception as e:
+        print ("Error",e)
+    time.sleep(10)
